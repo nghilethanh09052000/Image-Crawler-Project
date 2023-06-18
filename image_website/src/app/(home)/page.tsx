647537eefe39bb595c13fd19d9a19a17c308cda8
thanhnghi0905200 <nghilt19411@gmail.com>
2023-06-18 16:29:27 +0700

    add api

---
+++ image_website/src/app/(home)/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { connectMongoDb } from '@/utils/db'
+
+
+
+export default async function Home() {
+
+
+
+  return (
+   <div>
+    Nghi
+   </div>
+  )
+}
---
+++ image_website/src/utils/db.ts
@@ -0,0 +1,29 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export async function connectMongoDb() {
+  try 
+  {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = await client.db("images")
+    
+    console.log('Connect successfully')
+    return db
+
+  } 
+  catch(error) 
+  {
+    // Ensures that the client will close when you finish/error
+    console.log('Failed to connect', error)
+    await client.close();
+  }
+}
